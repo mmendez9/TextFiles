53a6f569ad5989a91a88af9396664a14150e4f5d
student <mmendez9@dtcc.edu>
2017-10-31 09:38:18 -0400

    Changing directories, reading and writing files

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import os
 
+#prompt user to change directory
 dirme = input("Enter a directory:".strip())
 os.chdir(dirme)
+listining = os.listdir(dirme)
+
+#Create file to write to
+output = open("write.txt", "w")
+
+#iterate throug directory
+for i in listining:
+    input = open(i, "r")
+    for lines in input:
+        output.write(lines)
+    input.close()
+    output.write("\n")
+output.close()
+
+#Remove files
+for i in listining:
+    if(i != "write.txt"):
+        os.remove(i)
